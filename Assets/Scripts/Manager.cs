ee02a800de98a25b3c9a8356799002940780d356
Brewknight <jmaliaras@gmail.com>
2021-04-25 17:39:15 +0300

    creating menu screen

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
+    public Camera GameCamera;
+    public GameObject GameLight;
+    public Camera MenuCamera;
+    public Canvas Menu;
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
+        GameCamera.gameObject.SetActive(false);
+        GameLight.SetActive(false);
+
+
+        MenuCamera.gameObject.SetActive(true);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void StartGame()
+    {
+
+    }
+
+    IEnumerator HideMenu()
+    {
+        yield break;
     }
 }
